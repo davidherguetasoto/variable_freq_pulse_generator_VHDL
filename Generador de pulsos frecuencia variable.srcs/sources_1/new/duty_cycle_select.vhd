65db09524fa9acfcd27d5aa73cff2bbcf984d08b
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2024-02-19 10:50:55 +0100

    implemented duty cycle select

--- Generador de pulsos frecuencia variable.srcs/sources_1/new/duty_cycle_select.vhd
+++ Generador de pulsos frecuencia variable.srcs/sources_1/new/duty_cycle_select.vhd
@@ -11,39 +11,75 @@ end duty_cycle_select;
 
 architecture Behavioral of duty_cycle_select is
 
-    signal reg:std_logic_vector(9 downto 0);
-    signal output:std_logic;
-    signal index:integer range 1 to 9;
+    signal reg1:std_logic_vector(9 downto 0);
+    signal output1:std_logic;
+    signal index1:integer range 1 to 9;
+    signal index_out1:integer range 1 to 9;
+    
+--    signal reg2:std_logic_vector(9 downto 0);
+--    signal output2:std_logic;
+--    signal index2:integer range 1 to 9;
+--    signal index_out2:integer range 1 to 9;
 
 begin
 
     output_generation:process(clk_in,reset)
     begin
         if reset='0' then 
-            reg<="1000000000";
-            output<='0';
-            index<=reg'left-1;
+            reg1<="1000000000";
+            output1<='0';
+            index1<=reg1'left;
+            index_out1<=reg1'left;
         elsif clk_in'event and clk_in='1' then 
-            output<=reg(9);
-            reg<=reg(8 downto 0)&reg(9);
+            if up='1' and index1>0 then
+                reg1(index1)<='1';
+                index1<=index1-1;
+                index_out1<=reg1'left;
+            elsif down='1' and index1<reg1'left then 
+                reg1(index1)<='0';
+                index1<=index1+1;
+                index_out1<=reg1'left;
+            else 
+                if index_out1>0 then 
+                    output1<=reg1(index_out1);
+                    index_out1<=index_out1-1;
+                else
+                    output1<=reg1(index_out1);
+                    index_out1<=reg1'left;
+                end if;
+            end if;  
         end if;
     end process;
+
+--FOR HALF WIDHT PULSES   
+--    output_generation2:process(clk_in,reset)
+--    begin
+--        if reset='0' then 
+--            reg2<="1000000000";
+--            output2<='0';
+--            index2<=reg2'left;
+--            index_out2<=reg2'left;
+--        elsif clk_in'event and clk_in='0' then 
+--            if up='1' and index2>0 then
+--                reg2(index2)<='1';
+--                index2<=index2-1;
+--                index_out2<=reg2'left;
+--            elsif down='1' and index2<reg2'left then 
+--                reg2(index2)<='0';
+--                index2<=index2+1;
+--                index_out2<=reg2'left;
+--            else 
+--                if index_out2>0 then 
+--                    output2<=reg2(index_out2);
+--                    index_out2<=index_out2-1;
+--                else
+--                    output2<=reg2(index_out2);
+--                    index_out2<=reg2'left;
+--                end if; 
+--            end if;  
+--        end if;
+--    end process;
     
-    duty_cycle_variation:process(clk_in,reset)
-    begin
-        if reset='0' then 
-            
-        elsif clk_in'event and clk_in='1' then 
-            if up='1' and index<reg'left-1 then
-                reg(index)<='1';
-                index<=index+1;
-            elsif down='1' and index>0 then 
-                reg(index)<='0';
-                index<=index-1;
-            end if;
-       end if;
-    end process;
-    
-    pulse_out<=output;
+    pulse_out<=output1; --and output2;
 
 end Behavioral;
