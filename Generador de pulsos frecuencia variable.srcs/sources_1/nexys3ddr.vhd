66ec6d17b3a2cccf21446dabf4a6be4bf704a725
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2024-02-19 12:46:20 +0100

    added first full implementation

--- Generador de pulsos frecuencia variable.srcs/sources_1/nexys3ddr.vhd
+++ Generador de pulsos frecuencia variable.srcs/sources_1/nexys3ddr.vhd
@@ -6,24 +6,43 @@ entity nexys3ddr is
     port(   
         JB: out std_logic_vector(7 downto 0);
         reset: in std_logic;
-        clk: in std_logic);
+        btnU: in std_logic;
+        btnL: in std_logic;
+        btnR: in std_logic;
+        btnD: in std_logic;
+        clk: in std_logic;
+        sw: in std_logic_vector(15 downto 0);
+        led: out std_logic_vector(15 downto 0));
 end nexys3ddr;
 
 architecture RTL of nexys3ddr is
 
-component pulse_generator is
-    port(
-        outputs: out std_logic_vector(7 downto 0);
-        clk100mhz:in std_logic;
-        reset:in std_logic);
-end component;
-
+    component top is
+    Port ( input_buttons: in std_logic_vector(3 downto 0);
+           reset : in STD_LOGIC;
+           clk100mhz : in STD_LOGIC;
+           freq_duty_selector : in STD_LOGIC;
+           freq_selector_fixed_clk : in STD_LOGIC_VECTOR (7 downto 0);
+           pulse_out : out STD_LOGIC);
+    end component;
+    
+    signal not_reset: std_logic;
+    
 begin
 
-    unit_pulse_generator: pulse_generator
+    unit_top: top
         port map(
-            outputs => JB,
-            reset => reset,
-            clk100mhz => clk);
+            input_buttons(0) => btnU,
+            input_buttons(1) => btnR,
+            input_buttons(2) => btnD,
+            input_buttons(3) => btnL,
+            reset => not_reset,
+            clk100mhz => clk,
+            freq_duty_selector => sw(0), 
+            freq_selector_fixed_clk => sw(15 downto 8),
+            pulse_out => JB(0));
 
+    led<=sw;
+    not_reset<= not reset;
+    
 end RTL;
--- Generador de pulsos frecuencia variable.srcs/sources_1/top.vhd
+++ Generador de pulsos frecuencia variable.srcs/sources_1/top.vhd
@@ -1,32 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 19.02.2024 11:11:16
--- Design Name: 
--- Module Name: top - RTL
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity top is
-    Port ( up : in STD_LOGIC;
-           up100 : in STD_LOGIC;
-           down : in STD_LOGIC;
-           down100 : in STD_LOGIC;
+    Port ( input_buttons: in std_logic_vector(3 downto 0);
            reset : in STD_LOGIC;
            clk100mhz : in STD_LOGIC;
            freq_duty_selector : in STD_LOGIC;
@@ -124,7 +100,97 @@ architecture RTL of top is
      );
      end component;
      
+     signal synchr_out: std_logic_vector(3 downto 0);
+     signal deb_out: std_logic_vector(3 downto 0);
+     signal edge_out: std_logic_vector(3 downto 0);
+     
+     signal clk50mhz: std_logic;
+     signal clk25mhz: std_logic;
+     signal clk10mhz: std_logic;
+     signal clk1mhz: std_logic;
+     signal clk100khz: std_logic;
+     signal clk10khz: std_logic;
+     signal clk1khz: std_logic;
+     
+     signal mux_8_1_out: std_logic;
+     
+     signal precharge: std_logic_vector(19 downto 0);
+     signal variable_freq_clk_out: std_logic;
+     signal variable_duty_cycle_clk_out: std_logic;
+     
 begin
 
+    input_buttons_conditioning: for i in 0 to 3 generate
+    inst_synchrnyzr: SYNCHRNZR port map(
+                            clk=>clk100mhz,
+                            async_in=>input_buttons(i),
+                            sync_out=>synchr_out(i),
+                            reset=>reset);
+                            
+    inst_debouncer: DEBOUNCER port map(
+                            CLK=>clk100mhz,
+                            btn_in=>synchr_out(i),
+                            btn_out=>deb_out(i),
+                            reset=>reset);
+                            
+    inst_edgedtctr: EDGEDTCTR port map(
+                            CLK=>clk100mhz,
+                            sync_in=>deb_out(i),
+                            edge=>edge_out(i),
+                            reset=>reset);
+    end generate input_buttons_conditioning;
+    
+    inst_fixed_clks: fixed_clks port map(
+                            clk100mhz => clk100mhz,
+                            reset => reset,
+                            clk100mhz_out => open,
+                            clk50mhz => clk50mhz,
+                            clk25mhz => clk25mhz,
+                            clk10mhz => clk10mhz,
+                            clk1mhz => clk1mhz,
+                            clk100kz => clk100khz,
+                            clk10kz => clk10khz,
+                            clk1khz => clk1khz);
+                            
+    inst_freq_selector: freq_selector port map(
+                            up10 => edge_out(0),
+                            up100 => edge_out(1),
+                            down10 => edge_out(2),
+                            down100 => edge_out(3),
+                            clk => clk100mhz,
+                            reset => reset,
+                            precharge => precharge);
+                            
+    inst_variable_frq_clk: variable_freq_clk port map(
+                            clk100mhz => clk100mhz,
+                            reset => reset,
+                            en => freq_selector_fixed_clk(0),
+                            precharge => precharge,
+                            var_clk => variable_freq_clk_out);
+                            
+    inst_one_hot_8bit_mux: one_hot_8bit_mux port map(
+                            input(0) => clk100mhz,
+                            input(1) => clk50mhz,
+                            input(2) => clk25mhz,
+                            input(3) => clk10mhz,
+                            input(4) => clk1mhz,
+                            input(5) => clk100khz,
+                            input(6) => clk10khz,
+                            input(7) => clk1khz,
+                            input_select => freq_selector_fixed_clk,
+                            output => mux_8_1_out);
+    
+    inst_variable_duty_cycle_clk: variable_duty_cycle_clk port map(
+                            clk_in => mux_8_1_out,
+                            reset => reset,
+                            pulse_out => variable_duty_cycle_clk_out,
+                            up => edge_out(0),
+                            down => edge_out(2));
+                            
+    inst_mux_2_1: mux_2_1 port map(
+                             input(0) => variable_freq_clk_out,
+                             input(1) => variable_duty_cycle_clk_out,
+                             input_select => freq_duty_selector,
+                             output => pulse_out);
 
 end RTL;
