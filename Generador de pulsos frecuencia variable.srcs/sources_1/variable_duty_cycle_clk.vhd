b5395bca95971372b13e5ea61f5f2e18effd7971
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2024-02-20 17:20:16 +0100

    Changing implementation of variable_duty_cycle clk due to errors while testing the board

--- Generador de pulsos frecuencia variable.srcs/sources_1/variable_duty_cycle_clk.vhd
+++ Generador de pulsos frecuencia variable.srcs/sources_1/variable_duty_cycle_clk.vhd
@@ -11,76 +11,93 @@ end variable_duty_cycle_clk;
 
 architecture Behavioral of variable_duty_cycle_clk is
 
-    signal reg1:std_logic_vector(9 downto 0);
-    signal output1:std_logic;
-    signal index1:integer range 1 to 9;
-    signal index_out1:integer range 1 to 9;
-
---FOR HALF WIDTH PULSES    
---    signal reg2:std_logic_vector(9 downto 0);
---    signal output2:std_logic;
---    signal index2:integer range 1 to 9;
---    signal index_out2:integer range 1 to 9;
+    signal reg_out:std_logic_vector(9 downto 0);
+    signal index:integer range 0 to 9;
+    
+    type state is (ResetState, MoveVector,Increment,Decrement);
+    signal next_state:state;
+    signal present_state:state;
 
 begin
 
-    output_generation:process(clk_in,reset)
-    begin
+     STATE_UPDATE:process(clk_in,reset)
+     begin
         if reset='0' then 
-            reg1<="1000000000";
-            output1<='0';
-            index1<=reg1'left;
-            index_out1<=reg1'left;
-        elsif clk_in'event and clk_in='1' then 
-            if up='1' and index1>0 then
-                reg1(index1)<='1';
-                index1<=index1-1;
-                index_out1<=reg1'left;
-            elsif down='1' and index1<reg1'left then 
-                reg1(index1)<='0';
-                index1<=index1+1;
-                index_out1<=reg1'left;
-            else 
-                if index_out1>0 then 
-                    output1<=reg1(index_out1);
-                    index_out1<=index_out1-1;
-                else
-                    output1<=reg1(index_out1);
-                    index_out1<=reg1'left;
-                end if;
-            end if;  
+            present_state<=ResetState;
+        elsif rising_edge(clk_in) then 
+            present_state<=next_state;
         end if;
-    end process;
-
---FOR HALF WIDHT PULSES   
---    output_generation2:process(clk_in,reset)
+     end process;
+     
+     STATE_TRANSITIONS:process(up,down,index)
+     begin 
+        case present_state is 
+            when ResetState=> 
+                
+           
+            when MoveVector=> 
+                
+                
+            when Increment=> 
+                
+                
+           when Decrement => 
+                
+        end case;
+    end process; 
+    
+    OUTPUT_GENERATION: process(present_state)
+    begin
+        case present_state is 
+            when ResetState => 
+                index<=reg_out'left;
+                reg_out<="1000000000";
+            
+            when MoveVector => 
+                reg_out <= reg_out(reg_out'left-1 downto 0)&reg_out(reg_out'left);
+                if index<9 then
+                    index<=index+1;
+                else 
+                    index<=0;
+                end if;
+            
+            when Increment => 
+            
+            when Decrement => 
+                index <= index -1;
+                
+         
+                 
+                    
+--    output_generation:process(clk_in,reset)
 --    begin
 --        if reset='0' then 
---            reg2<="1000000000";
---            output2<='0';
---            index2<=reg2'left;
---            index_out2<=reg2'left;
---        elsif clk_in'event and clk_in='0' then 
---            if up='1' and index2>0 then
---                reg2(index2)<='1';
---                index2<=index2-1;
---                index_out2<=reg2'left;
---            elsif down='1' and index2<reg2'left then 
---                reg2(index2)<='0';
---                index2<=index2+1;
---                index_out2<=reg2'left;
+--            reg1<="1000000000";
+--            output1<='0';
+--            index1<=reg1'left;
+--            index_out1<=reg1'left;
+--        elsif clk_in'event and clk_in='1' then 
+--            if up='1' and index1>0 then
+--                reg1(index1)<='1';
+--                index1<=index1-1;
+--                index_out1<=reg1'left;
+--            elsif down='1' and index1<reg1'left then 
+--                reg1(index1)<='0';
+--                index1<=index1+1;
+--                index_out1<=reg1'left;
 --            else 
---                if index_out2>0 then 
---                    output2<=reg2(index_out2);
---                    index_out2<=index_out2-1;
+--                if index_out1>0 then 
+--                    output1<=reg1(index_out1);
+--                    index_out1<=index_out1-1;
 --                else
---                    output2<=reg2(index_out2);
---                    index_out2<=reg2'left;
---                end if; 
+--                    output1<=reg1(index_out1);
+--                    index_out1<=reg1'left;
+--                end if;
 --            end if;  
 --        end if;
 --    end process;
+
     
-    pulse_out<=output1; --and output2;
+    pulse_out<=reg1(9); --and output2;
 
 end Behavioral;
